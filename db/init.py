919cbcd3c738c600a64aff30fb8d96ac51b60b33
ALiangLiang <me@aliangliang.top>
2024-04-08 08:19:41 +0000

    fix: use navgraph db to ensure full procedure data

--- db/init.py
+++ db/init.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
 
-engine = create_engine('sqlite:///little_navmap_msfs.sqlite').connect()
+engine = create_engine('sqlite:///little_navmap_navigraph.sqlite').connect()
 session = Session(engine)
 
 Base = declarative_base()
