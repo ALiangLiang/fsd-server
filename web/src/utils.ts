07ec9b4e5afef8e6925a20149dec21a71e799943
ALiangLiang <me@aliangliang.top>
2024-05-07 16:40:18 +0000

    fix: cannot request mic perm on first time

--- web/src/utils.ts
+++ web/src/utils.ts
@@ -14,15 +14,12 @@ export async function getMicStream(): Promise<MediaStream> {
   const getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia)
   const devices = await navigator.mediaDevices.enumerateDevices()
   const device = devices.find((device) => device.kind === 'audioinput' && device.deviceId === 'default')
-  if (!device) {
-    throw new Error('No default audio input device found')
-  }
   return new Promise((resolve, reject) => {
     getUserMedia(
       {
         video: false,
         audio: {
-          deviceId: device.deviceId,
+          deviceId: device?.deviceId,
           echoCancellation: { exact: false },
           noiseSuppression: { exact: true },
         }
